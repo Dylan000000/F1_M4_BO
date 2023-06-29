91a56fae3f1187955d50eaf07ab7905500221996
Dylan000000 <132989962+Dylan000000@users.noreply.github.com>
2023-06-29 10:06:53 +0200

    Dylan

---
+++ F1 M4 BO_unity/Assets/Github/scripts/PlatformMovement.cs
@@ -0,0 +1,62 @@
+using Pathfinding.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlatformMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform[] patrolPoints;
+    public float waitTime;
+    int currentPointIndex;
+    private Rigidbody2D rb;
+    private float horizontal;
+
+    bool once;
+
+    private void Start()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (transform.position != patrolPoints[currentPointIndex].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentPointIndex].position, speed * Time.deltaTime);
+
+
+        }
+        else
+        {
+            if (once == false)
+            {
+                once = true;
+                StartCoroutine(Wait());
+                
+            }
+        }
+
+
+
+
+
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (currentPointIndex + 1 < patrolPoints.Length)
+        {
+            currentPointIndex++;
+        }
+        else
+        {
+            currentPointIndex = 0;
+        }
+        once = false;
+    }
+
+
+
+}
